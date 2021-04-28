33d832fe8d7ec30469e574d9237971f5d648494e
Thai Le <ntle@castortech.com>
2021-04-28 11:36:20 -0400

    expose lib components, repositories and entities to springboot from mainApp

--- mainApp/src/main/java/demo/mainApp/MainAppApplication.java
+++ mainApp/src/main/java/demo/mainApp/MainAppApplication.java
@@ -2,8 +2,14 @@ package demo.mainApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"demo.lib1", "demo.lib2", "demo.lib3", "demo.mainApp"})
+@EnableJpaRepositories(basePackages = {"demo.lib1", "demo.lib2", "demo.lib3", "demo.mainApp"})
+@EntityScan(basePackages = {"demo.lib1", "demo.lib2", "demo.lib3", "demo.mainApp"})
 public class MainAppApplication {
 
 	public static void main(String[] args) {
